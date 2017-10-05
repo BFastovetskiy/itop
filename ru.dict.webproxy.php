e30ef2dad762bd828beaa1da8a45298d11fdbd1d
Boris Fastovetskiy <boris.fastovetskiy@gmail.com>
2017-10-05 17:37:39 +0300

    first commit

---
+++ ru.dict.webproxy.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+ Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:WebProxy' => 'Web прокси',
+	'Class:WebProxy+' => '',
+	'Class:WebProxy/Attribute:uplink_ipaddress' => 'IP адрес uplink',
+	'Class:WebProxy/Attribute:uplink_ipaddress+' => '',
+	'Class:WebProxy/Attribute:webapplication_list' => 'Web приложения',
+	'Class:WebProxy/Attribute:webapplication_list+' => '',
+	'Class:WebProxy/Attribute:webproxycluster_id' => 'Кластер',
+	'Class:WebProxy/Attribute:webproxycluster_id+' => '',
+	'Class:WebProxy/Attribute:redundancy' => 'Анализ влияния: конфигурация резервирования',
+	'Class:WebProxy/Attribute:redundancy/disabled' => 'Прикладное решение в работе, если все КЕ в работе',
+	'Class:WebProxy/Attribute:redundancy/count' => 'Прикладное решение в работе, если по крайней мере %1$s КЕ в работе',
+	'Class:WebProxy/Attribute:redundancy/percent' => 'Прикладное решение в работе, если по крайней мере %1$s %% КЕ в работе',
+	'Class:WebApplication/Attribute:webproxy_id' => 'Web прокси',
+	'Class:WebApplication/Attribute:webproxy_id+' => '',
+	'Class:LnkWebProxyToWebApplication' => 'Связь Web прокси с Web приложениями',
+	'Class:LnkWebProxyToWebApplication+' => '',
+	'Class:LnkWebProxyToWebApplication/Attribute:webproxy_id' => 'Web прокси',
+	'Class:LnkWebProxyToWebApplication/Attribute:webproxy_id+' => '',
+	'Class:LnkWebProxyToWebApplication/Attribute:webapplication_id' => 'Web приложения',
+	'Class:LnkWebProxyToWebApplication/Attribute:webapplication_id+' => '',
+	'Class:WebProxyCluster' => 'Кластер Web прокси',
+	'Class:WebProxyCluster+' => '',
+	'Class:WebProxyCluster/Attribute:webproxy_list' => 'Web прокси',
+	'Class:WebProxyCluster/Attribute:webproxy_list+' => '',
+	'Class:WebProxyCluster/Attribute:redundancy' => 'Анализ влияния: конфигурация резервирования',
+	'Class:WebProxyCluster/Attribute:redundancy/disabled' => 'Прикладное решение в работе, если все КЕ в работе',
+	'Class:WebProxyCluster/Attribute:redundancy/count' => 'Прикладное решение в работе, если по крайней мере %1$s КЕ в работе',
+	'Class:WebProxyCluster/Attribute:redundancy/percent' => 'Прикладное решение в работе, если по крайней мере %1$s %% КЕ в работе',
+	
+	
+));
+?>

1a9325a453d18082f8094eefd49ac28728ae31c5
Boris Fastovetskiy <boris.fastovetskiy@gmail.com>
2017-11-15 11:47:53 +0300

    The Request-Template module was translated into Russian language

---
+++ itop-request-template/ru.dict.itop-request-template.php
@@ -0,0 +1,33 @@
+<?php
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:ServiceSubcategory/Attribute:requesttemplates_list' => 'Шаблоны запросов',
+	'Class:ServiceSubcategory/Attribute:requesttemplates_list+' => '',
+));
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:RequestTemplate' => 'Шаблон запроса',
+	'Class:RequestTemplate+' => '',
+	'Class:RequestTemplate/Attribute:name' => 'Название',
+	'Class:RequestTemplate/Attribute:name+' => '',
+	'Class:RequestTemplate/Attribute:service_id' => 'Услуга',
+	'Class:RequestTemplate/Attribute:service_id+' => '',
+	'Class:RequestTemplate/Attribute:service_name' => 'Услуга',
+	'Class:RequestTemplate/Attribute:service_name+' => '',
+	'Class:RequestTemplate/Attribute:servicesubcategory_id' => 'Подкатегория услуги',
+	'Class:RequestTemplate/Attribute:servicesubcategory_id+' => '',
+	'Class:RequestTemplate/Attribute:servicesubcategory_name' => 'Подкатегория услуги',
+	'Class:RequestTemplate/Attribute:servicesubcategory_name+' => '',
+	'Class:RequestTemplate/Attribute:label' => 'Заголовок',
+	'Class:RequestTemplate/Attribute:label+' => '',
+	'Class:RequestTemplate/Attribute:description' => 'Описание',
+	'Class:RequestTemplate/Attribute:description+' => '',
+	'Class:UserRequest/Attribute:service_details' => 'Дополнительная информация',
+	'Class:UserRequest/Attribute:service_details+' => 'Additionnal information depending on the service',
+	'Class:Incident/Attribute:service_details' => 'Service details',
+	'Class:Incident/Attribute:service_details+' => 'Additionnal information depending on the service',
+));
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Menu:RequestTemplate' => 'Шаблоны запросов',
+	'Menu:RequestTemplate+' => 'Все шаблоны запросов',
+));

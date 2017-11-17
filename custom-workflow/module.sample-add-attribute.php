5a399f9f2cf9140570dd819ee3fed4c5ca21b777
Boris Fastovetskiy <boris.fastovetskiy@gmail.com>
2017-11-17 18:59:20 +0300

    Added iTop Workflow engine

---
+++ custom-workflow/module.sample-add-attribute.php
@@ -0,0 +1,52 @@
+<?php
+//
+// iTop module definition file
+//
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'sample-add-attribute/',
+	array(
+		// Identification
+		//
+		'label' => 'iTop Workflow engine',
+		'category' => 'business',
+
+		// Setup
+		//
+		'dependencies' => array(
+			
+		),
+		'mandatory' => false,
+		'visible' => true,
+
+		// Components
+		//
+		'datamodel' => array(
+			'model.sample-add-attribute.php'
+		),
+		'webservice' => array(
+			
+		),
+		'data.struct' => array(
+			// add your 'structure' definition XML files here,
+		),
+		'data.sample' => array(
+			// add your sample data XML files here,
+		),
+		
+		// Documentation
+		//
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => '', // hyperlink to more information, if any 
+
+		// Default settings
+		//
+		'settings' => array(
+			// Module specific settings go here, if any
+		),
+	)
+);
+
+
+?>

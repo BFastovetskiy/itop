caeb582e4e833648e1eb13b73a008b7735341236
Boris Fastovetskiy <boris.fastovetskiy@gmail.com>
2018-01-17 12:51:15 +0300

    Translating to Russian language

---
+++ itop-approval-portal/ru.dict.itop-approval-portal.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2018 @BorisFastovetskiy
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ * 
+ * @telegram	@BorisFastovetskiy
+ */
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Approval:Portal:Menu' => 'Согласования',
+	'Approval:Portal:Menu+' => 'Запросы, для которых ожидается ваше согласование',
+	'Approval:Portal:Title' => 'Запросы, ожидающие вашего согласования',
+	'Approval:Portal:Title+' => 'Пожалуйста, выберите запросы для согласования и используйте кнопки, расположенных в нижней части страницы.',
+	'Approval:Portal:NoItem' => 'В настоящее время нет запросов на согласование Вами',
+	'Approval:Portal:Dlg:Approve' => 'Подтвердите, что вы хотите согласовать <em><span class="approval-count">?</span></em> запросов',
+	'Approval:Portal:Dlg:Reject' => 'Подтвердите, что вы хотите  отклонить <em><span class="approval-count">?</span></em> запросов',
+	'Approval:Portal:Dlg:ApproveOne' => 'Подтвердите, что Вы хотите одобрить этот запрос.',
+	'Approval:Portal:Dlg:RejectOne' => 'Подтвердите, что Вы хотите отклонить этот запрос.',
+));
